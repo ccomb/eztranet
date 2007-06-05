9b1d1afcf93447af31073bcc30bfffd72a4a8c8c
Christophe Combelles <ccomb@free.fr>
2007-06-05 23:54:38 +0000

    modifs skin, permission, menu, etc.

--- eztranet/skin/browser.py
+++ eztranet/skin/browser.py
@@ -14,14 +14,12 @@ from interfaces import *
 class PageTitleContentProvider(object):
     u"""
     Un Content Provider qui permet d'afficher le titre de la page (dans le header html)
-    C'est important car ça aide le référencement
     """
     implements(IContentProvider)
     adapts(Interface, IDefaultBrowserLayer, Interface)
     def __init__(self, context, request, view):
-        self.context=context
+        self.context, self.request, self.view = context, request, view
         self._sitename = getSite().__name__
-
     def update(self):
         u"on récupère le nom du contexte s'il en a un"
         if hasattr(self.context,'__name__') and self.context.__name__ is not None:
@@ -30,3 +28,25 @@ class PageTitleContentProvider(object):
             self._pagetitle = self._sitename
     def render(self):
         return self._pagetitle
+
+class LogoProvider(object):
+    u"""
+    The view that provides the logo and accessories
+    """
+    implements(IContentProvider)
+    adapts(Interface, IDefaultBrowserLayer, Interface)
+    def __init__(self, context, request, view):
+        self.context, self.request, self.view = context, request, view
+    def __call__(self):
+        return self
+    def update(self):
+        try:
+            self.logo = getSite()['logo']
+        except:
+            self.logo = None
+        self.title = getSite().__name__
+        self.url = AbsoluteURL(self.logo, self.request)()
+    def render(self):
+        u"Instead of rendering HTML, we return a dict with what we want to be traversed in TALES"
+        return { 'url':self.url, 'title': self.title, 'alt':self.title }
+
