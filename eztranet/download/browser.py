c9d1f1e8051dba9d4bbf6551538540348fa51908
Christophe Combelles <ccomb@free.fr>
2007-06-15 08:35:40 +0000

    Envoi de la taille (content-length)

--- eztranet/download/browser.py
+++ eztranet/download/browser.py
@@ -2,7 +2,8 @@
 from zope.interface import Interface
 from zope.publisher.browser import BrowserView
 from zope.location.interfaces import ILocation
-from tempfile import TemporaryFile
+from tempfile import NamedTemporaryFile
+import os
 
 class DownloadView(BrowserView):
     u"""
@@ -13,9 +14,11 @@ class DownloadView(BrowserView):
         print type(self.context)
         if ILocation.providedBy(self.context):
             filename = self.context.__name__
-        tmpfile = TemporaryFile()
+        tmpfile = NamedTemporaryFile()
         tmpfile.write(self.context.data)
+        tmpfile.flush()
         self.request.response.setHeader('Content-disposition', 'attachment; filename=%s' % filename)
-        return tmpfile
+        self.request.response.setHeader('Content-length', os.path.getsize(tmpfile.name))
+        return tmpfile.file
 
 
