0e808eec8abda16582ec7195f3b0fbdfa623b16a
Christophe Combelles <ccomb@free.fr>
2008-07-20 12:23:48 +0000

    prepare for release 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='eztranet',
 
       # Fill in project info below
-      version='1.1dev',
+      version='1.1.0',
       description="",
       long_description="",
       keywords='eztranet extranet video photo',
