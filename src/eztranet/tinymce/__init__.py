70fcf9e92c85e094e85b019208604da26dd66583
Christophe Combelles <ccomb@free.fr>
2008-11-16 14:38:43 +0200

    Moved the js header to a template (to have the js cached)

--- src/eztranet/tinymce/__init__.py
+++ src/eztranet/tinymce/__init__.py
@@ -1,32 +1,29 @@
-from zope.schema.interfaces import IField
-from zope.schema import Text
+from zope.app.pagetemplate import ViewPageTemplateFile
 from zope.interface import implements
+from zope.schema import Text
+from zope.schema.interfaces import IField
 
 class IHtmlText(IField):
-    """An html text field"""
+    """An html text field
+    """
+
 
 class HtmlText(Text):
-    """Field describing an html text field"""
+    """Field describing an html text field
+    """
     implements(IHtmlText)
     
     def _validate(self, value):
         super(HtmlText, self)._validate(value)
 
+
 class TinyHeader(object):
+    """content provider for the tinymce js
+    """
+    template = ViewPageTemplateFile('header.pt')
+
     def update(self):
         pass
-    def render(self):
-        return """
-<script type="text/javascript" src="++resource++tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
-<script type="text/javascript">
-	tinyMCE.init({
-		mode : "textareas",
-		theme : "advanced",
-    theme_advanced_buttons1 :
-    "bold,italic,underline,strikethrough,|bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,|,blockquote,link,unlink,|,formatselect,fontsizeselect,|,code,|,forecolor,removeformat,hr",
-    theme_advanced_buttons2 : "",
-    theme_advanced_buttons3 : ""
 
-	});
-</script>
-"""
+    def render(self):
+        return self.template(self)
