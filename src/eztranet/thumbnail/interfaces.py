49e99e42876f644e157f2f961cbfbed0debdd986
Christophe Combelles <ccomb@free.fr>
2010-10-31 21:08:24 +0100

    let the thumbnails be blobs as well

--- src/eztranet/thumbnail/interfaces.py
+++ src/eztranet/thumbnail/interfaces.py
@@ -1,9 +1,11 @@
-from eztranet.config.interfaces import IConfigForm
 from zope.annotation.interfaces import IAttributeAnnotatable
 from zope.i18nmessageid import MessageFactory
 from zope.interface import Interface, Attribute
 from zope.schema import Int, Bool, Bytes
 _ = MessageFactory('eztranet')
+
+from eztranet.config.interfaces import IConfigForm
+from eztranet.project.interfaces import LargeBytes
 
 class IThumbnailed(IAttributeAnnotatable):
     """The marker interface to put on an object that must have a thumbnail"""
@@ -13,10 +15,10 @@ class IThumbnail(Interface):
     """Interface offered by an object that has a thumbnail.
     """
 
-    image = Bytes(title=_(u'Thumbnail'),
+    image = LargeBytes(title=_(u'Thumbnail'),
                   description=_(u'The Image object corresponding to the thumbnail'),
                   required=False)
-                  
+
     def compute_thumbnail(size):
         """compute a thumbnail with the given size"""
 
